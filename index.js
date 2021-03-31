2631437c7f6f55a5195350370da980da0376912e
gblanco <gustavo.blanco@tecsup.edu.pe>
2021-03-31 09:13:16 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+const inicio = (req,res) => {
+    res.end('<h1>Express inicio</h1>');
+}
+const saludo = (req,res) => {
+    res.end('<h1>Hola express</h1>');
+}
+
+app.get('/',inicio);
+app.get('/saludo',saludo);
+
+
+app.listen(3000, (req,res) => {
+    console.log('port:8000');
+});
